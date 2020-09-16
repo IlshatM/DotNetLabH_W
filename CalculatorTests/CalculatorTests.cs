22e98fb9f5e73563e14e2ae8222a12e2a26b8497
Ilshat Mukhamediarov <61285419+IlshatM@users.noreply.github.com>
2020-09-16 15:15:03 +0300

    Hw1 (#1)
    * Calculator: Done! Tests: ToDo
    
    * Calculator: Done! Tests: Done!
    
    * idea to ignore
    
    * HW1.csproj added again
    
    * 100% test cover achieved
    
    * Test Travis-Ci
    
    * Travis-Ci update test
    
    * Travis-ci update 2
    
    * Update .travis.yml
    
    * Update .travis.yml
    
    * Update .travis.yml
    
    * Update .travis.yml
    
    * Update .travis.yml
    
    * Update .travis.yml
    
    * Update .travis.yml
    
    * Update .travis.yml
    
    * Update .travis.yml
    
    * Update .travis.yml
    
    * Update .travis.yml
    
    * Update .travis.yml
    
    * Update .travis.yml
    
    * Update .travis.yml
    
    Co-authored-by: Max Arshinov <max@hightech.group>

---
+++ CalculatorTests/CalculatorTests.cs
@@ -0,0 +1,36 @@
+using HW1;
+using NUnit.Framework;
+
+namespace CalculatorTests
+{
+    [TestFixture]
+    public class CalculatorTestCase
+    {
+        [TestCase]
+        public void Program_Main_Res_Return4()
+        {
+            Program.Main(new string[] { });
+            Assert.AreEqual(4, Program.Res);
+        }
+        [TestCase]
+        public void Calculate_Plus_2_3_Returned5()
+        {
+            Assert.AreEqual(5, Calculator.Calculate("2+3"));
+        }
+        [TestCase]
+        public void Calculate_Minus_2_3_ReturnedMinus1()
+        {
+            Assert.AreEqual(-1, Calculator.Calculate("2-3"));
+        }
+        [TestCase]
+        public void Calculate_Multiply_2_3_Returned6()
+        {
+            Assert.AreEqual(6, Calculator.Calculate("2*3"));
+        }
+        [TestCase]
+        public void Calculate_Devide_10_5_Returned2()
+        {
+            Assert.AreEqual(2, Calculator.Calculate("10/5"));
+        }
+    }
+}
---
+++ HW1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace HW1
+{
+    
+    public class Program
+    {
+        public static double Res;
+        public static void Main(string[] args)
+        {
+            Res = Calculator.Calculate("2+3-1");
+            Console.WriteLine(Res);
+        }
+    }
+}
