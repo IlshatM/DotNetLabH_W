e3899e845eaecc703a6f04b8072638ca84f9b163
IlshatM <61285419+IlshatM@users.noreply.github.com>
2020-09-27 23:42:49 +0300

    Changed approach

--- FsharpCalculator/Program.fs
+++ FsharpCalculator/Program.fs
@@ -1,49 +1,39 @@
-﻿// Learn more about F# at http://fsharp.org
-
-open System
+﻿open System
 open System.Collections.Generic
-let removeAt index input =
-  input 
-  |> List.mapi (fun i el -> (i <> index, el)) 
-  |> List.filter fst |> List.map snd
-let stringFromCharList (cl : char list) = //+
+let stringFromCharList (cl : char list) = 
     String.concat "" <| List.map string cl
-let ClearString(s:string):string = //+
+let ClearString(s:string):string = 
     let chars = Seq.toList s
-    let mutable pure_str = ""
     let pure_chars = List.filter(fun x->Char.IsDigit(x)=true || x='+' || x='-' || x='*' || x='/') chars
     stringFromCharList(pure_chars)
-let Split (str:string) (ch:char) = str.Split([|ch|], StringSplitOptions.None) |> Array.toList//+
-let GetNumber(s:string):double = //+
+let Split (str:string,ch:char) = str.Split([|ch|], StringSplitOptions.None) |> Array.toList
+let GetNumber(s:string):double = 
     s|>double
-let NumberDev(s:string):double = 
-    let str = Split s '/'
-    let mutable res = GetNumber(str.Head)
-    let str = removeAt 0 str
-    List.iter(fun x->(res<-res/GetNumber(x))) str
-    res
-let NumberMult(s:string):double =
-    let str = Split s '*'
-    let mutable res = NumberDev(str.Head)
-    let str = removeAt 0 str
-    List.iter(fun x->(res<-res*NumberDev(x))) str
-    res    
-let NumberMinus(s:string) =
-    let str = Split s '-'
-    let mutable res = NumberMult(str.Head)
-    let str = removeAt 0 str
-    List.iter(fun x->(res<-res-NumberMult(x))) str
-    res        
-let NumberPlus(s_raw:string) =
-    let s = ClearString(s_raw)
-    let str = Split s '+'
-    let mutable res = 0.0
-    List.iter(fun x->(res<-res+NumberMinus(x))) str
-    res
+let bindPlus(s:string) =
+    (s, '+')
+let Calculate(expression:string) =
+    expression
+            |> ClearString
+            |> bindPlus
+            |> Split 
+            |> List.map(fun t->Split(t, '-'))
+            |> List.map(fun t -> List.map(fun t1->Split(t1,'*'))t) 
+            |> List.map(fun t->List.map(fun t1 -> List.map(fun t2 -> Split(t2, '/'))t1)t)
+            |> List.map(fun t->List.map(fun t1->List.map(fun t2->List.map(fun t3->GetNumber(t3))t2)t1)t)
+            |> List.map(fun t->List.map(fun t1->List.map(fun t2->List.reduce(/) t2)t1)t)
+            |> List.map(fun t1->List.map(fun t2->List.reduce(*) t2)t1)
+            |> List.map(fun t2->List.reduce(-) t2)
+            |> List.reduce(+)
+    
+            
+        
+    
 
-    
-    
+
 [<EntryPoint>]
 let main argv =
-    printfn "%f" (NumberPlus("3/2"))
+    printfn "%f" (Calculate("2+3*2 - 11/11 "))
+    
+    
+
     0 // return an integer exit code
