f8c1247e9c86e2b7ff0a11b93589b7edf66bbe35
IlshatM <61285419+IlshatM@users.noreply.github.com>
2020-10-24 01:17:14 +0300

    Tree half ready

--- CalcExpProxy/Program.cs
+++ CalcExpProxy/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,109 +8,95 @@ using System.Net;
 using System.Net.Http;
 using System.Reflection;
 using System.Reflection.Emit;
+using System.Reflection.Metadata;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
-using HW1;
 
 namespace CalcExpProxy
 {
+    class MyVisitor : ExpressionVisitor
+    {
+        protected override Expression VisitConstant(ConstantExpression node)
+        {
+            if (node.Type == typeof(string))
+            {
+                if(Convert.ToString(node.Value).Contains('+'))
+                {
+                    List<Expression> args = new List<Expression>();
+                    foreach (var i in Convert.ToString(node.Value).Split('+'))
+                    {
+                        args.Add(Expression.Constant(i,typeof(string)));
+                    }
+
+                    for (int i = 0; i < args.Count; i++)
+                    {
+                        args[i] = this.Visit(args[i]);
+                    }
+                    var res = 
+                        Expression.Call(typeof(Calculator).GetMethod(nameof(Calculator.Root)),Expression.NewArrayInit(typeof(double),args));
+                    
+                    return res;
+                }
+                if (Convert.ToString(node.Value).Contains('-'))
+                {
+                    List<Expression> args = new List<Expression>();
+                    foreach (var i in Convert.ToString(node.Value).Split('-'))
+                    {
+                        args.Add(Expression.Constant(i,typeof(string)));
+                    }
+
+                    for (int i = 0; i < args.Count; i++)
+                    {
+                        args[i] = this.Visit(args[i]);
+                    }
+                    var res = 
+                        Expression.Call(typeof(Calculator).GetMethod(nameof(Calculator.MinusRoot)),Expression.NewArrayInit(typeof(double),args));
+                    return res;
+                }
+                else
+                {
+                    return Expression.Call(typeof(Calculator).GetMethod(nameof(Calculator.GetReq)),
+                        Expression.Constant(Convert.ToString(node.Value),
+                            typeof(string)
+                        )
+                    );
+                }
+
+            }
+            return node;
+        }
+        
+    }
     
     static class Calculator
     {
+        public static double Root(params double[] mas)
+        {
+            var res = 0.0;
+            foreach (var i in mas)
+            {
+                res = GetReq($"{res}+{i}");
+            }
+            return res;
+        }
+        public static double MinusRoot(params double[] mas)
+        {
+            var res = mas[0] * 2;
+            foreach (var i in mas)
+            {
+                res = GetReq($"{res}-{i}");
+            }
+            
+            return res;
+        }
         
         public static double GetReq(string expression)
         {
             WebRequest myReq =
                 WebRequest.Create($"https://localhost:5001/calculate?expression={ConvertExpression(expression)}");
             return Convert.ToDouble(myReq.GetResponse().Headers["calculator_result"]);
-            
         }
-
-        public static double FinalCalculate(List<double> mas, List<char> act)
-        {
-            var res = mas[0];
-            
-            for(int i = 1;i<mas.Count;i++)
-            {
-                if (act[i - 1] == '+')
-                {
-                    res = GetReq($"{res}+{mas[i]}");
-                }else if (act[i - 1] == '-')
-                {
-                    res = GetReq($"{res}-{mas[i]}");
-                }
-            }
-
-            return res;
-        }
-        public static List<double> SimpleExpressionsCalculate(List<string> expr)
-        {
-            List<double> res = new List<double>();
-            foreach (var i in expr)
-            {
-                res.Add(SimpExpr(i));
-            }
-
-            return res;
-        }
-
-        public static double SimpExpr(string expr)
-        {
-            StringBuilder num = new StringBuilder();
-            int i = 0;
-            while (Char.IsDigit(expr[i]))
-            {
-                num.Append(expr[i]);
-                
-                i++;
-                if(i>=expr.Length) break;
-            }
-
-            double res = Convert.ToDouble(num.ToString());
-            num.Clear();
-            if (i < expr.Length)
-            {
-                num.Append(expr[i]);
-
-                expr += ' ';
-                for (int j = i + 1; j < expr.Length; j++)
-                {
-                    if (!Char.IsDigit(expr[j]))
-                    {
-                        res = GetReq($"{res}{num}");
-                        num.Clear();
-                    }
-
-                    num.Append(expr[j]);
-                }
-            }
-
-            return res;
-        }
-
-        public static List<char> GetActs(string expression)
-        {
-            List<char> actions = new List<char>();
-            
-            foreach (var i in expression)
-            {
-                if (i == '+' || i == '-')
-                {
-                    actions.Add(i);
-                }
-            }
-
-            return actions;
-        }
-        public static List<string> Split(string expression)
-        {
-            List<string> expressions = new List<string>();
-
-            expressions = expression.Split('+', '-').ToList();
-            return expressions;
-        }
-        
         private static string ConvertExpression(string expression)
         {
             expression = expression.Replace("+", "%2B");
@@ -117,20 +104,20 @@ namespace CalcExpProxy
             expression = expression.Replace("/", "%2F");
             return expression;
         }
-
-        public static Expression expr = Expression.Parameter(typeof(string), "expr");
-        public static Expression splt = Expression.Call(typeof(Calculator).GetMethod("Split"), expr);
-        public static Expression getactions = Expression.Call(typeof(Calculator).GetMethod("GetActs"),expr);
-        public static Expression simple_expression = 
-            Expression.Call(typeof(Calculator).GetMethod("SimpleExpressionsCalculate"), splt);
-        public static Expression final_calculation = 
-            Expression.Call(typeof(Calculator).GetMethod("FinalCalculate"), simple_expression, getactions);
+        
+        public static Expression CreateTree(string expr)
+        {
+            Expression start = Expression.Constant(expr,typeof(string));
+            MyVisitor mv = new MyVisitor();
+            var res = mv.Visit(start);
+            return res;
+        }
     }
     class Program
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine(Calculator.CreateTree("2+3*4-1+11-4/2"));
         }
     }
 }
